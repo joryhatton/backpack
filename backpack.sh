e18310b6ed7f1bd3460ccde5c5b564d57a4b12b5
Jory Hatton <joryhatton@gmail.com>
2018-09-22 11:59:28 -0500

    improvements

--- backpack.sh
+++ backpack.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo
 echo ' _                 _                       _     '
 echo '| |               | |                     | |    '
 echo '| | _   ____  ____| |  _ ____   ____  ____| |  _ '
@@ -7,22 +6,19 @@ echo '| || \ / _  |/ ___) | / )  _ \ / _  |/ ___) | / )'
 echo '| |_) | ( | ( (___| |< (| | | ( ( | ( (___| |< ( '
 echo '|____/ \_||_|\____)_| \_) ||_/ \_||_|\____)_| \_)'
 echo '                        |_|                      '
-
 echo
-echo
-echo
-echo
-echo '-- updating system --'
 echo
 
+echo '#### updating system ####'
+echo
 sudo apt-get update
 sudo apt-get upgrade
+echo
+echo '#### DONE ####'
+echo
+echo
 
-echo
-echo
-echo
-echo
-echo '-- installing core packages --'
+echo '#### installing core packages ####'
 echo
 
 sudo apt-get install build-essential
@@ -36,56 +32,59 @@ sudo apt-get install tmux
 sudo apt-get install libpq-dev
 sudo apt-get install redis
 sudo apt-get install neofetch
+echo
+echo '#### DONE ####'
+echo
+echo
 
-echo
-echo
-echo
-echo
-echo '-- set up nodejs --'
+echo '#### set up nodejs ####'
 echo
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
+echo
+echo '#### DONE ####'
+echo
+echo
 
-echo
-echo
-echo
-echo
-echo '-- set up zsh --'
+echo '#### set up zsh ####'
 echo
 
 if [ -d ~/.oh-my-zsh ]
 then
-  echo 'ZSH ALREADY INSTALLED'
+  echo 'NOTE: zsh already configured'
 else
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   chsh -s "$(which zsh)"
   rm ~/.zshrc
   ln -fs ~/dotfiles/zshrc ~/.zshrc
 fi
-
 echo
+echo '#### DONE ####'
 echo
-echo
-echo
-echo '-- set up vim --'
 echo
 
-mkdir ~/.vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo '#### set up vim ####'
+echo
+
+if [ -d ~/.vim/autoload ]
+then
+  echo 'NOTE: vim already configured'
+else
+  mkdir ~/.vim
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
 if [ -f ~/dotfiles/vimrc ]
 then
   ln -fs ~/dotfiles/vimrc ~/.vimrc
 fi
-echo 'set up vim'
+echo
+echo '#### DONE ####'
+echo
+echo
 
-
-echo
-echo
-echo
-echo
-echo ' --set up github --'
+echo '#### set up git ####'
 echo
 
 if [ -f ~/dotfiles/gitconfig ]
@@ -95,7 +94,7 @@ fi
 
 if [ -f ~/.gitconfig.local ]
 then
-  echo 'GIT ALREADY CONFIGURED'
+  echo 'NOTE: git already configured'
 else
   echo '[user]' >> ~/.gitconfig.local
   echo 'What is your full name used on GitHub?'
@@ -110,38 +109,48 @@ else
 fi
 
 echo
+echo '#### DONE ####'
 echo
 echo
-echo
-echo '-- set up remaining config files --'
+
+echo '#### set up tmux ####'
 echo
 
 if [ -f ~/dotfiles/tmux.conf  ]
 then
   ln -fs ~/dotfiles/tmux.conf ~/.tmux.conf
 fi
-echo 'DONE'
+
+if [ -d ~/.tmux/plugins ]
+then
+  echo 'NOTE: tmux already configured'
+else
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+tmux source ~/.tmux.conf
 
 echo
+echo '#### DONE ####'
 echo
 echo
-echo
-echo '-- set up ruby --'
+
+echo '#### set up ruby ####'
 echo
 
 if [ -d ~/.rbenv ]
 then
-  echo 'RBENV ALREADY INSTALLED'
+  echo 'NOTE: ruby already configured'
 else
   git clone https://github.com/rbenv/rbenv.git ~/.rbenv
   mkdir -p "$(rbenv root)"/plugins
   git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 fi
+echo
+echo '#### DONE ####'
+echo
+echo
 
-echo
-echo
-echo
-echo
 echo ' _                 _                       _     '
 echo '| |               | |                     | |    '
 echo '| | _   ____  ____| |  _ ____   ____  ____| |  _ '
@@ -151,3 +160,4 @@ echo '|____/ \_||_|\____)_| \_) ||_/ \_||_|\____)_| \_)'
 echo '                        |_|                      '
 echo
 echo "Thanks for using backpack! You're all set!"
+echo 'Contribute to the project at https://github.com/fulstop/backpack'
