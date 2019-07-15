7bf53229a5e1566228f2829bc55bccf81c0f1589
Jory Hatton <joryhatton@gmail.com>
2019-07-15 09:46:06 -0500

    cleaning up

--- backpack.sh
+++ backpack.sh
@@ -7,19 +7,9 @@ echo '| |_) | ( | ( (___| |< (| | | ( ( | ( (___| |< ( '
 echo '|____/ \_||_|\____)_| \_) ||_/ \_||_|\____)_| \_)'
 echo '                        |_|                      '
 echo
-echo
 
-echo '#### updating system ####'
-echo
 sudo apt-get update
 sudo apt-get upgrade
-echo
-echo '#### DONE ####'
-echo
-echo
-
-echo '#### installing core packages ####'
-echo
 
 sudo apt-get install build-essential
 sudo apt-get install vim
@@ -32,113 +22,80 @@ sudo apt-get install libpq-dev
 sudo apt-get install zsh
 sudo apt-get install tmux
 sudo apt-get install redis
+sudo apt-get install redis-server
 sudo apt-get install neofetch
 sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
-echo
-echo '#### DONE ####'
-echo
-echo
 
-echo '#### set up nodejs ####'
-echo
+# start postgres and redis
+sudo service postgresql restart
+sudo service redis-server restart
+sudo update-rc.d postgresql enable
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
-echo
-echo '#### DONE ####'
-echo
-echo
+# vim
+if [ -f ~/.vimrc ]
+  echo 'Vim already configured'
+then
+  # ripgrep
+  curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb
+  sudo dpkg -i ripgrep_0.10.0_amd64.deb
+  rm ripgrep_0.10.0_amd64.deb
+  ln -fs ~/dotfiles/vimrc ~/.vimrc
+fi
 
-echo '#### set up vim ####'
-echo
-
+# vim
 if [ -d ~/.vim/autoload ]
 then
-  echo 'NOTE: vim already configured'
+  echo 'Vim already configured'
 else
   mkdir ~/.vim
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-if [ -f ~/dotfiles/vimrc ]
-then
-  ln -fs ~/dotfiles/vimrc ~/.vimrc
-  # install ripgrep
-  curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb
-  sudo dpkg -i ripgrep_0.10.0_amd64.deb
-  rm ripgrep_0.10.0_amd64.deb
-fi
-
-echo
-echo '#### DONE ####'
-echo
-echo
-
-echo '#### set up tmux ####'
-echo
-
-if [ -f ~/dotfiles/tmux.conf  ]
+# tmux
+if [ -f ~/tmux.conf  ]
+  echo 'Tmux already configured'
 then
   ln -fs ~/dotfiles/tmux.conf ~/.tmux.conf
+  tmux source ~/.tmux.conf
 fi
 
+# tmux
 if [ -d ~/.tmux/plugins ]
 then
-  echo 'NOTE: tmux already configured'
+  echo 'Tmux already configured'
 else
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
-tmux source ~/.tmux.conf
-
-echo
-echo '#### DONE ####'
-echo
-echo
-
-echo '#### set up ruby ####'
-echo
-
+# ruby
 if [ -d ~/.rbenv ]
 then
-  echo 'NOTE: ruby already configured'
+  echo 'Ruby already configured'
 else
   git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-  mkdir -p "$(rbenv root)"/plugins
-  git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+  mkdir -p ~/.rbenv/plugins
+  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 fi
-echo
-echo '#### DONE ####'
-echo
 
-echo '#### set up zsh ####'
-echo
-
-if [ -d ~/.rbenv ]
+# zsh
+if [ -f ~/.zhsrc ]
 then
-  echo 'NOTE: ruby already configured'
+  echo 'Zsh already configured'
 else
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   chsh -s "$(which zsh)"
   rm ~/.zshrc
   ln -fs ~/dotfiles/zshrc ~/.zshrc
 fi
-echo
-echo '#### DONE ####'
-echo
 
-echo '#### cleaning up ####'
-echo
-
+# nodejs
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 sudo apt-get clean
 sudo apt-get autoremove
 
 echo
-echo '#### DONE ####'
-echo
-echo
-
 echo ' _                 _                       _     '
 echo '| |               | |                     | |    '
 echo '| | _   ____  ____| |  _ ____   ____  ____| |  _ '
@@ -149,4 +106,6 @@ echo '                        |_|                      '
 echo
 echo "Thanks for using backpack! You're all set!"
 echo 'Contribute to the project at https://github.com/fulstop/backpack'
+
 source ~/.zshrc
+reload
