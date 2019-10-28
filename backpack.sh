03d5948204e3ee61ce048f0444cf1562ce516e23
Jory Hatton <joryhatton@gmail.com>
2019-10-28 20:35:40 +0000

    add docker

--- backpack.sh
+++ backpack.sh
@@ -30,6 +30,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
+# and docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # free tds
 wget http://www.freetds.org/files/stable/freetds-1.1.6.tar.gz
