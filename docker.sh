0821592d33c7ba3740574b0282decb4fbd4544c2
Jory Hatton <joryhatton@gmail.com>
2019-12-31 15:25:41 -0600

    clean up docker stuff

--- docker.sh
+++ docker.sh
@@ -5,12 +5,12 @@ sudo apt-get update -y
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 sudo apt-get -y purge docker docker-engine docker.io containerd runc
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update -y
-sudo apt-get install -y docker-ce
-sudo usermod -aG docker $USER
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
 
 # and docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
